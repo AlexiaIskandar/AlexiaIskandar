c885efd20ca5e27ca7561d512cb363dfdc05b1dd
AlexiaIskandar <indratresna666@gmail.com>
2024-05-19 02:57:33 +0800

    Update README.py

--- README.py
+++ README.py
@@ -40,8 +40,3 @@ void OnTick()
      }
   }
 //+------------------------------------------------------------------+
-
-<!---
-AlexiaIskandar/AlexiaIskandar is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
